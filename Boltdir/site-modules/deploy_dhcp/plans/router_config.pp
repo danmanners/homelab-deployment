9295fbcc22f0cac59dd6705d9d799bb7bdb61de6
Dan Manners <daniel.a.manners@gmail.com>
2020-06-05 23:43:27 -0400

    Make sure Qemu-Guest-Agent is installed and running.

--- Boltdir/site-modules/deploy_dhcp/plans/router_config.pp
+++ Boltdir/site-modules/deploy_dhcp/plans/router_config.pp
@@ -6,4 +6,18 @@ plan deploy_dhcp::router_config(
 ) {
   # Prep the server
   apply_prep('bastion')
+
+  # Install Qemu-Guest-Agent and ensure the service is enabled and started.
+  apply('bastion', _run_as => root) {
+
+    package {'qemu-guest-agent':
+      ensure => 'installed', 
+    }
+
+    service {'qemu-service':
+      name => 'qemu-guest-agent',
+      ensure => true,
+      enable => true,
+    }
+  }
 }
