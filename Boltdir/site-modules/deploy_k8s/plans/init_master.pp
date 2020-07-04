e9ceb737da5b9ff2d8c63e3ae72d0b5908c0288d
Dan Manners <daniel.a.manners@gmail.com>
2020-07-04 10:30:43 -0400

    Updated the kubernetes setup to ensure that sysctl values persist after reboots.

--- Boltdir/site-modules/deploy_k8s/plans/init_master.pp
+++ Boltdir/site-modules/deploy_k8s/plans/init_master.pp
@@ -3,24 +3,39 @@ plan deploy_k8s::init_master () {
   apply_prep(['k8s-primary'])
 
   apply('k8s-primary', _run_as => root) {
-# Ensures the conntrack count value is set to 0.  
+  # Ensures the conntrack count value is set to 0.  
     sysctl { 'net.netfilter.nf_conntrack_count':
       ensure  => present,
       value   => '0',
       comment => 'Required for Kubernetes',
     }
-    # SonarQube and Elasticsearch Requirements
+    # SonarQube and Elasticsearch Requirements.
     sysctl {'vm.max_map_count':
       ensure  => present,
-      value   => 262144,
+      value   => $vm_max_map_count,
       comment => 'Required for various Kubernetes services',
     }
     sysctl {'fs.file-max':
       ensure  => present,
-      value   => 65536,
+      value   => $fs_file_max,
       comment => 'Required for various Kubernetes services',
     }
-# Use the Ubuntu Hiera file and provision the Kubernetes controller.
+
+    # Ensure the sysctl settings are persistent.
+    file_line{'persistent-vm.max_map_count':
+      ensure => present,
+      path   => '/etc/sysctl.conf',
+      line   => "vm.max_map_count = ${vm_max_map_count}",
+      match  => '^vm.max_map_count = '
+    }
+    file_line{'persistent-fs.file-max':
+      ensure => present,
+      path   => '/etc/sysctl.conf',
+      line   => "fs.file-max = ${fs_file_max}",
+      match  => '^fs.file-max = '
+    }
+
+    # Use the Ubuntu Hiera file and provision the Kubernetes controller.
     class {'kubernetes':
       controller => true,
       require    => Sysctl['net.netfilter.nf_conntrack_count']
--- Boltdir/site-modules/deploy_k8s/plans/init_workers.pp
+++ Boltdir/site-modules/deploy_k8s/plans/init_workers.pp
@@ -3,23 +3,41 @@ plan deploy_k8s::init_workers () {
   apply_prep(['k8s-nodes'])
 
   apply('k8s-nodes', _run_as => root) {
-# Ensures the conntrack count value is set to 0.  
+
+    $vm_max_map_count = 262144
+    $fs_file_max = 65536
+
+  # Ensures the conntrack count value is set to 0.  
     sysctl { 'net.netfilter.nf_conntrack_count':
       ensure  => present,
       value   => '0',
       comment => 'Required for Kubernetes',
     }
 
-    # SonarQube and Elasticsearch Requirements
+    # SonarQube and Elasticsearch Requirements.
     sysctl {'vm.max_map_count':
       ensure  => present,
-      value   => 262144,
+      value   => $vm_max_map_count,
       comment => 'Required for various Kubernetes services',
     }
     sysctl {'fs.file-max':
       ensure  => present,
-      value   => 65536,
+      value   => $fs_file_max,
       comment => 'Required for various Kubernetes services',
+    }
+
+    # Ensure the sysctl settings are persistent.
+    file_line{'persistent-vm.max_map_count':
+      ensure => present,
+      path   => '/etc/sysctl.conf',
+      line   => "vm.max_map_count = ${vm_max_map_count}",
+      match  => '^vm.max_map_count = '
+    }
+    file_line{'persistent-fs.file-max':
+      ensure => present,
+      path   => '/etc/sysctl.conf',
+      line   => "fs.file-max = ${fs_file_max}",
+      match  => '^fs.file-max = '
     }
 
 # Use the Ubuntu Hiera file and provision the Kubernetes controller.
